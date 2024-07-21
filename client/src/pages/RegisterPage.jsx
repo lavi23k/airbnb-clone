0318eec0b728f15a39ef4f7ba6193d64e342e711
Vusal <vusalcs54@gmail.com>
2024-07-21 20:57:50 +0400

    third commit

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios"
 
 export default function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function registerUser(ev) {
+      ev.preventDefault();
+      axios.post('/register', {
+          name,
+          email,
+          password,
+      });
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-md mx-auto">
-          <input type="text" placeholder="Vusal Abdulkarimov"/>
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+          <input type="text" placeholder="Vusal Abdulkarimov" value={name} onChange={ev => setName(ev.target.value)}/>
+          <input type="email" placeholder="your@email.com" value={email} onChange={ev => setEmail(ev.target.value)}/>
+          <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Allready a member? <Link className="underline text-black" to={"/login"}>Login</Link>
           </div>
