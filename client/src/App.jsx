adee02c4080b10d86653333dac4d5084c1d1c13f
Vusal <vusalcs54@gmail.com>
2024-07-25 00:51:54 +0400

    Update API and App, add JWT for authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage.jsx';
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://localhost:4000"
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
