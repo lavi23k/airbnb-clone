17aa4a11e3643412035fd8664243ec77083f2cd1
Vusal <vusalcs54@gmail.com>
2024-07-29 22:44:09 +0400

    seventh commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage.jsx';
+import AccountPage from './pages/AccountPage.jsx';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext.jsx';
 
@@ -18,6 +19,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage?" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
