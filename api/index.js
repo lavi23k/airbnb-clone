a75491af48a78c531c9fcb77f6aeedbf75ef1ea0
Vusal <vusalcs54@gmail.com>
2024-07-29 22:44:09 +0400

    Update API and client, create Account page

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 const cookieParser = require("cookie-parser");
-require("dotenv").config()
+require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:5173',
+  credentials: true,
+  origin: 'http://localhost:5173',
 }));
 
 app.options('*', cors());
@@ -24,57 +24,59 @@ app.options('*', cors());
 mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req, res) => {
-    res.json('test ok');
+  res.json('test ok');
 });
 
 app.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    try {
-        const userDoc = await User.create({
-            name,
-            email,
-            password: bcrypt.hashSync(password, bcryptSalt),
-        });
-        res.json(userDoc);
-    } catch (e) {
-        res.status(422).json(e);
-    }
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
 });
 
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const userDoc = await User.findOne({ email });
-    if (userDoc) {
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        jwt.sign({ email: userDoc.email, id: userDoc._id}, jwtSecret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token', token).json(userDoc);
-          });
-      } else {
-        res.status(422).json('pass not ok');
-      }
-    } else {
-      res.json('not found');
-    }
-  });
-  
-
-  app.get('/profile', async (req, res) => {
-    const { token } = req.cookies;
-    if (token) {
-      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
         if (err) throw err;
-        const { name, email, _id } = await User.findById(userData.id);
-        res.json({ name, email, _id });
+        res.cookie('token', token).json(userDoc);
       });
     } else {
-      res.json(null);
+      res.status(422).json('pass not ok');
     }
-  }); 
-  
+  } else {
+    res.json('not found');
+  }
+});
+
+app.get('/profile', async (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
 
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+  console.log('Server is running on port 4000');
 });
