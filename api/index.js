58e3d1bbf3f24420afc15b0ed40d64158ec08166
Vusal <vusalcs54@gmail.com>
2024-08-03 17:53:39 +0400

    eleventh commit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 const cookieParser = require("cookie-parser");
+const imageDownloader  = require("image-downloader");
 require("dotenv").config();
 const app = express();
 
@@ -13,6 +14,7 @@ const jwtSecret = "fase57wawaw8";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(cors({
   credentials: true,
@@ -75,6 +77,16 @@ app.get('/profile', async (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const {link} = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image ({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
 });
 
 app.listen(4000, () => {
