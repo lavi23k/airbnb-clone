b68ef4f503d22f44bf426acc9d90c7df7efdbf12
Vusal <vusalcs54@gmail.com>
2024-08-04 03:40:52 +0400

    Add multi-photo upload support and enhance perks handling

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 const cookieParser = require("cookie-parser");
 const imageDownloader  = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs");
 require("dotenv").config();
 const app = express();
 
@@ -88,6 +90,21 @@ app.post("/upload-by-link", async (req, res) => {
   });
   res.json(newName);
 });
+
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(newPath.replace('uploads\\', '').replace('uploads/', ''));
+    }
+    res.json(uploadedFiles);
+});
+
 
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
