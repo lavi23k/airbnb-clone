e5113d5b3b2797f29d09d7a0c5b7ae596284d754
Vusal <vusalcs54@gmail.com>
2024-08-09 22:40:43 +0400

    Add individual place page with dynamic photo display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import RegisterPage from './pages/RegisterPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import PlacesPage from './pages/PlacesPage.jsx';
 import PlacesFormPage from './pages/PlacesFormPage.jsx';
+import PlacePage from './pages/PlacePage.jsx';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext.jsx';
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
           <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id" element={<PlacePage />} />
         </Route>
       </Routes>
     </UserContextProvider>
